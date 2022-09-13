1b7f94b11f3ba92d60f41d46588c59668d108526
wsdonnelly <wsdonnelly@gmail.com>
2022-09-13 16:44:20 +0300

    working draft

--- tori_scraper.js
+++ tori_scraper.js
@@ -1,114 +1,65 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
 const path = require('node:path')
+const prompt = require('prompt-sync')()
 fs = require('fs')
 
-const prompt = require('prompt-sync')();
+//get search term
+const SEARCH_TERM = prompt('Enter a search term (replace spaces with a "+"): ')
+var PAGE_NUM = 2
 
-
-
-const SEARCH_TERM = prompt('Enter a search term: ')
-//const SEARCH_TERM = 'baby toy'
-let PAGE_NUM = 0
-
-
-
-
-
-
-
-//download imgs to folder
+//create dir
 var dir = `./${SEARCH_TERM}_jpgs`
 if (!fs.existsSync(dir)){
 	fs.mkdirSync(dir)
 }
 
-for (PAGE_NUM; PAGE_NUM < 100; PAGE_NUM++) {
-	axios.get(`https://www.tori.fi/koko_suomi?ca=18&q=${SEARCH_TERM}&w=3&o=${PAGE_NUM}`)
-		.then ((res) => {
-			const $ = cheerio.load(res.data)
+//generic function to download a single image
+const downloadImage  = async (url, path) => {
+	try {
+		const response = await axios({
+			method: "get",
+			url: url,
+			responseType: "stream",
+			timeout: 20000
+		})
+		response.data.pipe(fs.createWriteStream(path))
+		console.log("downloading image")
+	} catch (err) {
+		console.log('download error')
+	}
+	
+}
 
-			$('.item_image').each((idx, el) => {
+const getAll = async () => {
+	try {
+		const res = await axios.get(`https://www.tori.fi/koko_suomi?q=${SEARCH_TERM}&cg=0&w=3&st=s&st=k&st=u&st=h&st=g&ca=18&l=0&md=th`, {timeout: 10000})
+		const $ = cheerio.load(res.data)
+		const result = $('#last_page').find('a').attr('href')
+		const re = /(?<=o=).*/
+		const MAX_PAGE = parseInt(result.match(re)[0])
+
+		$('.item_image').each((idx, el) => {
 				let url = $(el).attr('src')
-				const name = path.basename(url)
-				axios({
-					method: "get",
-					url: url,
-					responseType: "stream"
-				})
-				.then((response) => {
-					response.data.pipe(fs.createWriteStream(`${dir}/${name}.jpg`));
-				})
-				.catch((err) => console.log(err))
-			})
+		const name = path.basename(url)
+			downloadImage(url, `${dir}/${name}.jpg`)
 		})
-}
-
-
-
-
-
-
-/*
-//download single img
-const fileUrl = "https://images.tori.fi/api/v1/imagestori/images/100130472406.jpg?rule=thumb_280x210"
-const name = path.basename(fileUrl)
-console.log(name)
-axios({
-	method: "get",
-	url: fileUrl,
-	responseType: "stream"
-}).then((response) => {
-	response.data.pipe(fs.createWriteStream(`images/${name}.jpg`));
-})
-
-
-
-//more complete version
-export async function downloadFile(fileUrl: string, outputLocationPath: string) {
-  const writer = createWriteStream(outputLocationPath);
-
-  return Axios({
-    method: 'get',
-    url: fileUrl,
-    responseType: 'stream',
-  }).then(response => {
-
-    //ensure that the user can call `then()` only when the file has
-    //been downloaded entirely.
-
-    return new Promise((resolve, reject) => {
-      response.data.pipe(writer);
-      let error = null;
-      writer.on('error', err => {
-        error = err;
-        writer.close();
-        reject(err);
-      });
-      writer.on('close', () => {
-        if (!error) {
-          resolve(true);
-        }
-        //no need to call the reject here, as it will have been called in the
-        //'error' stream;
-      });
-    });
-  });
-}
-
-
-//write img url's to file
-for (PAGE_NUM; PAGE_NUM < 100; PAGE_NUM++) {
-	axios.get(`https://www.tori.fi/koko_suomi?ca=18&q=${SEARCH_TERM}&w=3&o=${PAGE_NUM}`)
-		.then ((res) => {
-			const $ = cheerio.load(res.data)
-
-			$('.item_image').each((idx, el) => {
-				let img = $(el).attr('src')
-				fs.writeFile('tori_imgs_src.txt', img + "\n", {flag: 'a+'}, (err) => {
-					if (err) return console.log(err)
+		for(PAGE_NUM; PAGE_NUM <= MAX_PAGE; PAGE_NUM++) {
+			try {
+				const result = await axios.get(`https://www.tori.fi/koko_suomi?ca=18&q=${SEARCH_TERM}&w=3&o=${PAGE_NUM}`, {timeout: 10000})
+				const $ = cheerio.load(result.data)
+	//		console.log(`found ${$('.item_image').length} images`)
+				$('.item_image').each((idx, el) => {
+					let url = $(el).attr('src')
+					const name = path.basename(url)
+					downloadImage(url, `${dir}/${name}.jpg`)
 				})
-			})
-		})
+			} catch (err) {
+					console.log("download error")
+			}
+		}
+		} catch (err) {
+			console.log ("error on first page")
+		}
 }
-*/
+getAll()
