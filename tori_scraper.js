ddd399ab40c3b08eb0be2a0be2de7ef421a54fcd
wsdonnelly <wsdonnelly@gmail.com>
2022-09-15 11:03:34 +0300

    working but messy draft of the complete scraper

--- tori_scraper.js
+++ tori_scraper.js
@@ -26,58 +26,68 @@ const downloadImage  = async (url, path) => {
 		response.data.pipe(fs.createWriteStream(path))
 		console.log("downloading image")
 	} catch (err) {
-		console.log('download error')
+		console.log('download error in function')
 	}
 	
 }
 
 const goToLink = async (url) => {
-	const res = await axios.get(url)
+	try {
+		const uri = encodeURI(url)
+		const res = await axios.get(uri)
+		const $ = cheerio.load(res.data)
 
-	const $ = cheerio.load(res.data)
+		$('.thumb_link').each((idx, el) => {
+			let url = $(el).attr('href')
+			//console.log('download link', url)
+			const name = path.basename(url)
+			downloadImage(url, `${dir}/${name}.jpg`)
+		})
+	} catch (err) {
+		console.log(err)
+	}
 
-	$('.thumb_link').each((idx, el) => {
-		let url = $(el).attr('href')
-		console.log(url)
-		const name = path.basename(url)
-		downloadImage(url, `${dir}/${name}.jpg`)
-	})
 }
 
 const getAll = async () => {
 	try {
 		//get data from first page
-		const res = await axios.get(`https://www.tori.fi/koko_suomi?q=${SEARCH_TERM}&cg=0&w=3&st=s&st=k&st=u&st=h&st=g&ca=18&l=0&md=th`, {timeout: 10000})
-		let $ = cheerio.load(res.data)
-		const result = $('#last_page').find('a').attr('href')
+		var res = await axios.get(`https://www.tori.fi/koko_suomi?q=${SEARCH_TERM}&cg=0&w=3&st=s&st=k&st=u&st=h&st=g&ca=18&l=0&md=th`, {timeout: 10000})
+		var $ = cheerio.load(res.data)
+		var result = $('#last_page').find('a').attr('href')
 		const re = /(?<=o=).*/
 		const MAX_PAGE = parseInt(result.match(re)[0])
 
-		const list = $('.list_mode_thumb').html()
+		var list = $('.list_mode_thumb').html()
 		$ = cheerio.load(list)
 		$("a").each((idx, el) => {
-			const item_url = $(el).attr('href')
-			console.log("HERES THE LINK",item_url)
+			var item_url = $(el).attr('href')
+		//	console.log("HERES THE first page LINK",item_url)
 			goToLink(item_url)
 		})
-		/*
+		console.log ("max page = ", MAX_PAGE)
 		for(PAGE_NUM; PAGE_NUM <= MAX_PAGE; PAGE_NUM++) {
+			console.log('HERE')
 			try {
-				const result = await axios.get(`https://www.tori.fi/koko_suomi?ca=18&q=${SEARCH_TERM}&w=3&o=${PAGE_NUM}`, {timeout: 10000})
-				const $ = cheerio.load(result.data)
-	//		console.log(`found ${$('.item_image').length} images`)
-				$('.item_image').each((idx, el) => {
-					let url = $(el).attr('src')
-					const name = path.basename(url)
-				//	downloadImage(url, `${dir}/${name}.jpg`)
+				res = await axios.get(`https://www.tori.fi/koko_suomi?ca=18&q=${SEARCH_TERM}&w=3&o=${PAGE_NUM}`, {timeout: 20000})
+				$ = cheerio.load(res.data)
+				list = $('.list_mode_thumb').html()
+				$ = cheerio.load(list)
+				$("a").each((idx, el) => {
+					item_url = $(el).attr('href')
+					if (!item_url.includes('autot')) {
+				//		console.log(`HERES THE page ${PAGE_NUM} LINK`, item_url)
+						goToLink(item_url)
+					}
 				})
 			} catch (err) {
-					console.log("download error")
+				console.log("error in loop")
 			}
-		}
-		*/
-		} catch (err) {
-			console.log ("error on first page")
-		}
+	}
+	}catch (err) {
+		console.log ("error on first page")
+	}
+
 }
+
 getAll()
